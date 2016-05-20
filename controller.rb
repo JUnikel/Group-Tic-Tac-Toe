0c65cb4b3c5aefb50f8d6783e32cd21dc7a467d2
Jackson Unikel <jaxu10@gmail.com>
2016-05-20 13:36:07 -0500

    something

---
+++ controller.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+require 'sinatra'
+require_relative 'models/TTT.rb'
+enable :sessions
+
+get '/' do
+	@game = Game.new
+	session[:game] = @game
+	erb :welcome
+end
+
+get '/p1' do
+	@game = session[:game]
+	if @game.p2win== true
+		erb :results
+	else
+		erb :p1
+	end
+end
+
+post '/p1' do
+	@game = session[:game]
+	@game.player2(params[:grid_position]) 
+	if @game.p2win== true
+		erb :results
+	else
+		erb :p1
+	end
+end
+
+post '/p2' do
+	@game = session[:game]
+	@game.player1(params[:grid_position]) 
+	if @game.p1win == true
+		erb :results
+	else
+		erb :p2
+	end
+end
